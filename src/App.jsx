bf2a3cf8f4f2cea1c99bdc235d6d1a061cf175b7
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-05-31 17:32:52 -0700

    Created Results component with dummy content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import UserInput from './components/UserInput';
+import Results from './components/Results';
 
 function App() {
   return (
     <>
       <Header />
       <UserInput />
+      <Results />
     </>
   )
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,7 @@
+function Results({ investmentNumbers }) {
+  return (
+    <section id="results" className="center">results go here</section>
+  );
+}
+
+export default Results;
